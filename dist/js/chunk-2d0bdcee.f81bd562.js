edad2ccf4e707cbe74b50c2770010ab7a829d8db
NaiHua Chen <vip899629@gmail.com>
2021-07-02 17:33:28 +0800

    add dist folder

---
+++ dist/js/chunk-2d0bdcee.f81bd562.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bdcee"],{"2e27":function(n,e,t){"use strict";function r(n,e,t,r,i,d){return" 單一頁面 "}t.r(e);var i={data:function(){return{id:""}},created:function(){var n=this.$route.params.id;this.id=n}};i.render=r;e["default"]=i}}]);
+//# sourceMappingURL=chunk-2d0bdcee.f81bd562.js.map
