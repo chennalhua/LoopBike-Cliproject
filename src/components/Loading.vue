f14dcffaebdd4cb7ee85acd53e5a2c4ed4a054fb
NaiHua Chen <vip899629@gmail.com>
2021-07-08 21:54:13 +0800

    [後台小功能]新增

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="loading-bg position-absolute vh-100">
+        <div class="spinner-border position-absolute loading-box" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loading: true
+        };
+    },
+}
+</script>
--- src/components/admin/Pagination.vue
+++ src/components/admin/Pagination.vue
@@ -3,17 +3,19 @@
     <ul class="pagination m-0">
       <li class="page-item">
           <a class="page-link" href="#"
-          @click.prevent="$emit('getProducts',page.current_page-1)">Previous</a>
+          @click.prevent="$emit('updatePage',page.current_page-1)">
+          <i class="bi bi-arrow-left"></i></a>
       </li>
       <li class="page-item" v-for="(item) in page.total_pages" :key="item"
       :class="{'active': item === page.current_page}">
-          <a class="page-link" href="#" @click.prevent="$emit('getProducts',item)">
+          <a class="page-link" href="#" @click.prevent="$emit('updatePage',item)">
               {{item}}
           </a>
         </li>
       <li class="page-item">
           <a class="page-link" href="#"
-          @click.prevent="$emit('getProducts',page.current_page+1)">Next</a>
+          @click.prevent="$emit('updatePage',page.current_page+1)">
+          <i class="bi bi-arrow-right"></i></a>
       </li>
     </ul>
   </nav>
